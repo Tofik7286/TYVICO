8d86b5c4f69094f7e8056b5020e57db83319b903
tofik <tofik.imscit21@gmail.com>
2026-09-03 12:12:55 +0530

    Vercel Analytics Added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Work from './components/Work';
 import About from './components/About';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 /**
  * TYVICO Landing Page
@@ -51,6 +52,8 @@ export default function App() {
         <CTA />
       </main>
       <Footer />
+      <Analytics />
+
     </>
   );
 }
